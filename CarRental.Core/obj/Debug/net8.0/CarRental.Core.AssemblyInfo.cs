eb8708250bb4d36b6df7655bf9e5d1b5731db132
AwadNasr <awadnasr21@gmail.com>
2024-04-20 03:33:42 +0200

    lastversion

--- CarRental.Core/obj/Debug/net8.0/CarRental.Core.AssemblyInfo.cs
+++ CarRental.Core/obj/Debug/net8.0/CarRental.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1b08f0450543b97ac29e419ca2fbb7ca35db11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba54fae299416f6e01d9fe8a040f89560944af1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
